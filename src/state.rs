3d7038e394199e1a97be80076aa7ffbb8a6a9f06
kyunghyunHan <ekfqlwcjswl@naver.com>
2023-07-21 16:44:44 +0900

    feat:strucrt

--- src/state.rs
+++ src/state.rs
@@ -1,13 +1,23 @@
-use  borsh::{BorshDeserialize, BorshSerialize};
-use solana_program::{
-    account_info::AccountInfo,
-    msg,
-    program_error::PrintProgramError,
-    program_pack::{IsInitialized,Pack,Sealed},
-    pubkey::Pubkey,
+use {
+    borsh::{BorshDeserialize, BorshSerialize},
+    solana_program::{
+        account_info::AccountInfo,
+        msg,
+        program_error::ProgramError,
+        program_pack::{IsInitialized, Pack, Sealed},
+        pubkey::Pubkey,
+    },
 };
+
 
 #[derive(Clone, Debug, BorshSerialize, BorshDeserialize, PartialEq)]
 pub struct NameRecordHeader {
-    
+
+    pub parent_name: Pubkey,
+
+ 
+    pub owner: Pubkey,
+
+
+    pub class: Pubkey,
 }
